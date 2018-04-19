3d10d2e0491fda78f94d4cc339471657153eabe3
Nardier <nardierbarbosa@gmail.com>
2018-04-19 19:33:14 -0300

    2° inicio - conceito e pre aplicação

--- OrdenacaoAlgoritimos/src/pages/home/home.ts
+++ OrdenacaoAlgoritimos/src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { AboutPage } from '../about/about';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  aboutPage: any;
+  
   constructor(public navCtrl: NavController) {
-
+    this.aboutPage = AboutPage;
   }
-
+  Estrutura(){
+    this.navCtrl.push(this.aboutPage, {
+            userData: "xxx"
+        });
+  }
 }
