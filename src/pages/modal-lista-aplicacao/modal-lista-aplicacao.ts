57b9b4cf740df37973d58a47ba5fe63fab7b89ed
Kelvenrafa <kelvenrafaelrbp@hotmail.com>
2018-04-26 19:59:45 -0300

    Feito por cara de chewbacca -Berg Lispector

--- src/pages/modal-lista-aplicacao/modal-lista-aplicacao.ts
+++ src/pages/modal-lista-aplicacao/modal-lista-aplicacao.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the ModalListaAplicacaoPage page.
@@ -15,7 +15,50 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class ModalListaAplicacaoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  public num: any;
+  public lista: any;
+
+  constructor(private alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+    this.lista = new Array();
+  }
+
+  insert(){
+    this.lista.push(this.num);
+    this.num = null;
+  }
+
+  remove(){
+    if(this.lista.length > 0){
+      var idx = this.lista.findIndex(element => element == this.num);
+      if(idx === -1){
+        let alert = this.alertCtrl.create({
+        title: 'Falha!',
+        subTitle: 'O elemento não foi encontrado!',
+        buttons: ['Ok']
+        });
+        alert.present();
+      } else {
+        this.lista[idx] = null;
+        let arrayAux = new Array();
+        arrayAux = this.lista;
+        this.lista = [];
+        for(let i = 0; i < arrayAux.length; i++){
+          if(arrayAux[i] != null){
+            this.lista.push(arrayAux[i]);
+          }
+        }
+        this.num = null;
+      }
+
+    } else {
+      let alert = this.alertCtrl.create({
+      title: 'Falha!',
+      subTitle: 'Lista está vazia, você não pode retirar!',
+      buttons: ['Ok']
+      });
+      alert.present();
+    }
+
   }
 
   ionViewDidLoad() {
