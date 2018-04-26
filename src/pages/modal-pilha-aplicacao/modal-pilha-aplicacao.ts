83befa374da7b45fc8e8180f55f8ac7345f20fd6
Kelvenrafa <kelvenrafaelrbp@hotmail.com>
2018-04-26 01:51:50 -0300

    Implementação da aplicação da pilha

--- src/pages/modal-pilha-aplicacao/modal-pilha-aplicacao.ts
+++ src/pages/modal-pilha-aplicacao/modal-pilha-aplicacao.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { Component, Pipe } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the ModalPilhaAplicacaoPage page.
@@ -8,6 +8,10 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
  * Ionic pages and navigation.
  */
 
+@Pipe({
+  name: 'reverse'
+})
+
 @IonicPage()
 @Component({
   selector: 'page-modal-pilha-aplicacao',
@@ -15,7 +19,35 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class ModalPilhaAplicacaoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  public topo: any = 0;
+  public num: any;
+  public pilha: any;
+
+  constructor(private alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+    this.pilha = new Array();
+  }
+
+  insert(){
+    this.pilha.reverse();
+    this.pilha.push(this.num);
+    this.num = null;
+    this.pilha.reverse();
+  }
+
+  remove(){
+    if(this.pilha.length > 0){
+      this.pilha.reverse();
+      this.pilha.pop();
+      this.pilha.reverse();
+    } else {
+      let alert = this.alertCtrl.create({
+      title: 'Falha!',
+      subTitle: 'Pilha está vazia, você não pode retirar!',
+      buttons: ['Ok']
+      });
+      alert.present();
+    }
+
   }
 
   ionViewDidLoad() {
