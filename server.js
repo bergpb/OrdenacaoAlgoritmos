b450592d84908a52549275c4179558e779fa03ea
Berg Paulo <lindembergdepaulo@gmail.com>
2018-05-14 20:00:35 -0400

    Update server.js
    server editado

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express'),
     app = express();
 
-app.use('/', express.static(__dirname + '/'));
+app.use(express.static('www'));
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
